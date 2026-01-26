cec86ac2f6a8a3f70bfa7ac77c20c718de243bdb
aidantimmyd4 <aidantimmyd4@gmail.com>
2026-01-25 22:51:42 -0700

    Lab 2 - Exercise 1c

---
+++ Lab_2/exercise_1c.m
@@ -0,0 +1,51 @@
+% Controls Lab 2 - 1c
+% Donnellan - 01/26
+% This code is provided from 1a written by Google Gemini Pro with some
+% variable changes to suit the differential equation derivations
+
+function controls_lab2_1c()
+
+% Define Variables
+m1 = 5; % kg
+m2 = 5; % kg
+b1 = 1; % N s/m
+k1 = 5; % N/m
+k2 = 5; % N/m
+
+% Time span
+tspan = [0 30]; % 0-30 seconds
+
+% Initial conditions
+z0 = [0; 0; 0; 0]; % [x1; dx1; x2; dx2] (set to 0; 0; 0; 0 for no initial conditions)
+
+% Solve ODE
+[t, z] = ode45(@(t, z) dynamics(t, z, m1, m2, b1, k1, k2), tspan, z0);
+
+% Plot
+figure;
+plot(t, z(:,1), 'R'); 
+hold on;
+plot(t, z(:, 3), 'G');
+xlabel('Time(s)')
+ylabel('Displacement (m)')
+title('Coupled System Response to Step Input y(t)');
+legend('x_1 (Mass 1)', 'x_2 (Mass 2)');
+grid on; 
+
+end 
+
+function dzdt = dynamics(t, z, m1, m2, b1, k1, k2)
+    f = sin(t);
+    x1 = z(1);
+    dx1 = z(2);
+    x2 = z(3);
+    dx2 = z(4);
+
+    % State Equations
+    dz1 = dx1;
+    dz2 = (1/m1) * (-b1*(dx2-dx1) - k1*x1 + k2*(x2 - x1));
+    dz3 = dx2;
+    dz4 = (1/m2) * (-k2*(x2 - x1) - b1*(dx2-dx1) + f);
+
+    dzdt = [dz1; dz2; dz3; dz4];
+end
